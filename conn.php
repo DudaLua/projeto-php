bda8b4a7a8af656a9a1693bbb9b99cb348fc484e
FabrSilva <fasillva056@gmail.com>
2019-06-11 18:32:48 -0300

    adicionando a conexao

---
+++ conn.php
@@ -0,0 +1,15 @@
+<?php
+$conn = new mysqli('localhost','aluno','aluno','root','festival');
+
+if (mysqli_connect_error)){
+
+    echo 'Error de conexao';
+
+}else{
+        echo 'Deu certo pivete'
+    
+}
+Localhost/pr
+
+
+
